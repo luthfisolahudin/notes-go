d2baefadbe941b193367ea68e8a100f442f5d2f8
Luthfi Solahudin <luthfisolahudin@outlook.com>
2024-03-09 09:03:17 +0700

    wip: new command

--- main.go
+++ main.go
@@ -7,32 +7,32 @@ import (
 	"path/filepath"
 	"regexp"
 	"strconv"
-	"strings"
 	"time"
 
+	"dario.cat/mergo"
 	"github.com/BurntSushi/toml"
 	"github.com/urfave/cli/v2"
 )
 
-type Config struct {
-	Daily struct {
+type (
+	Config struct {
+		Categories map[string]Category `toml:"categories"`
+	}
+
+	Category struct {
+		Editor    string `toml:"editor"`
+		Stubs     string `toml:"stubs"`
 		Path      string `toml:"path"`
 		Filename  string `toml:"filename"`
 		Extension string `toml:"ext"`
-	} `toml:"daily"`
+	}
+)
 
-	Project struct {
-		Path      string `toml:"path"`
-		Filename  string `toml:"filename"`
-		Extension string `toml:"ext"`
-	} `toml:"project"`
-}
-
-func parseDate(value string) (time.Time, error) {
+func ParseDate(value string) (time.Time, error) {
 	datePattern := "(?P<Date>(?:0?[1-9])|(?:[12][1-9])|(?:3[01]))"
 	monthPattern := "(?P<Month>(?:0?[1-9])|(?:1[0-2]))"
 	yearPattern := "(?P<Year>(?:[0-9]{2})|(?:[0-9]{4}))"
-	separatorPattern := "(?:[ -])"
+	separatorPattern := "(?:[.-_/])"
 	now := time.Now().Local()
 
 	re := regexp.MustCompile("^" + datePattern + "$")
@@ -68,54 +68,132 @@ func parseDate(value string) (time.Time, error) {
 	return time.Time{}, errors.New("unrecognized date")
 }
 
-func main() {
-	const DefaultConfigPath = "./config.toml"
-	var config Config
+func (c Config) ResolveCategory(name string) (Category, error) {
+	category, found := c.Categories[name]
 
-	if _, err := toml.DecodeFile(DefaultConfigPath, &config); err != nil {
-		log.Panic(err)
+	if !found {
+		return Category{}, errors.New("category not found")
 	}
+
+	defaultCategory, found := c.Categories["default"]
+
+	if !found {
+		return Category{}, errors.New("default category not found")
+	}
+
+	mergo.Merge(&category, defaultCategory)
+
+	return category, nil
+}
+
+func main() {
+	var configPath string
+	var config Config
+	var silent bool
 
 	app := &cli.App{
 		Name:  "notes",
 		Usage: "Manage personal notes",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "config",
+				Aliases:     []string{"C"},
+				Value:       "./config.toml",
+				Destination: &configPath,
+			},
+			&cli.BoolFlag{
+				Name:        "silent",
+				Aliases:     []string{"s"},
+				Value:       false,
+				Destination: &silent,
+			},
+		},
+		Before: func(ctx *cli.Context) error {
+			if _, err := toml.DecodeFile(configPath, &config); err != nil {
+				if !silent {
+					log.Fatalln(err)
+				} else {
+					return errors.New("failed decode config")
+				}
+			}
+
+			return nil
+		},
 		Commands: []*cli.Command{
 			{
-				Name:      "daily",
-				Aliases:   []string{"d"},
-				Usage:     "Generate daily note",
-				ArgsUsage: "[date]",
+				Name:    "new",
+				Aliases: []string{"n"},
+				Usage:   "Generate new note",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:    "category",
+						Aliases: []string{"c"},
+						Value:   "default",
+					},
+					&cli.StringFlag{
+						Name:    "sub-directory",
+						Aliases: []string{"D"},
+					},
+					&cli.StringFlag{
+						Name:        "date",
+						Aliases:     []string{"d"},
+						DefaultText: "today",
+					},
+					&cli.BoolFlag{
+						Name:    "first-directory",
+						Aliases: []string{"f"},
+					},
+					&cli.BoolFlag{
+						Name:    "new",
+						Aliases: []string{"n"},
+					},
+					&cli.BoolFlag{
+						Name:    "open-editor",
+						Aliases: []string{"e"},
+					},
+				},
 				Action: func(ctx *cli.Context) error {
+                    var category Category
+                    var err error
+
+					if category, err = config.ResolveCategory(ctx.String("category")); err != nil {
+						if !silent {
+							log.Fatalln(errors.New("category not found"))
+						} else {
+							return nil
+						}
+					}
+
 					var date time.Time
 
-					if ctx.Args().Present() {
-						var err error
-
-						if date, err = parseDate(strings.Join(ctx.Args().Slice(), " ")); err != nil {
-							log.Panic(err)
-
-							return nil
+					if ctx.String("date") != "" {
+						if date, err = ParseDate(ctx.String("date")); err != nil {
+							if !silent {
+								log.Fatalln(err)
+							} else {
+								return nil
+							}
 						}
 					} else {
 						date = time.Now().Local()
 					}
 
-					path := filepath.Join(config.Daily.Path, date.Format(config.Daily.Filename)+config.Daily.Extension)
+					path := filepath.Join(category.Path, date.Format(category.Filename)+category.Extension)
 					folder := filepath.Dir(path)
 
 					if _, err := os.Stat(path); !errors.Is(err, os.ErrNotExist) {
-						log.Fatal(errors.New("note already exist"))
+						return errors.New("note already exist")
 					}
 
 					if err := os.MkdirAll(folder, os.ModePerm); err != nil {
-						log.Panic(err)
+						log.Fatalln(err)
 					}
 
 					if _, err := os.Create(path); err != nil {
-						log.Panic(err)
+						log.Fatalln(err)
 					}
 
-					log.Print("note " + path +  " created")
+					log.Println("note " + path + " created")
 
 					return nil
 				},
@@ -124,6 +202,6 @@ func main() {
 	}
 
 	if err := app.Run(os.Args); err != nil {
-		log.Panic(err)
+		log.Fatalln(err)
 	}
 }
